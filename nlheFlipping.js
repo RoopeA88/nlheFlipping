4ac4b5ef9a070f06d11e953522b7b48b6b5a2f80
Roope <roope.aalto88@gmail.com>
2021-10-21 21:21:16 +0300

    continue add stuff

--- nlheFlipping.js
+++ nlheFlipping.js
@@ -5,7 +5,7 @@ function dealCards(){
     
     return player;
 }
-
+// tarkista, ettei boardiin tule samoja kortteja, nyt voi tulla
 function dealBoard(){
     let cards = [2,3,4,5,6,7,8,9,"T","J","Q","K","A"];
     let suits = ["of Clubs","of Hearts","of Spades","of Diamonds"];
@@ -19,9 +19,15 @@ function dealBoard(){
 // console.log(dealCards());
 // console.log(dealBoard());
 
+function hands(playerCard1,playerCard2,computerCard1, computerCard2, board){
+
+}
+
 function play(){
     let playerCard1 = "";
     let playerCard2 = "";
+    let computerCard1 = "";
+    let computerCard2 = "";
     let board = "";
     while(true){
         playerCard1 = dealCards();
@@ -31,12 +37,21 @@ function play(){
         }
     }
     while(true){
+        computerCard1 = dealCards();
+        computerCard2 = dealCards();
+        if(computerCard1 != computerCard2 && computerCard1 != playerCard1 && computerCard1 != playerCard2 && computerCard2 != playerCard1 
+            && computerCard2 != playerCard2){
+                break;
+        }
+    }
+    while(true){
         board = dealBoard();
-        if(!board.includes(playerCard1) && !board.includes(playerCard2)){
+        if(!board.includes(playerCard1) && !board.includes(playerCard2) && !board.includes(computerCard1) && !board.includes(computerCard2)){
             break;
         }
     }
-    console.log(playerCard1+" | "+playerCard2);
+    console.log("Players hole cards: "+playerCard1+" | "+playerCard2);
+    console.log("Computers hole cards: "+computerCard1+" | "+computerCard2);
     console.log("-----------");
     console.log(board);
     
